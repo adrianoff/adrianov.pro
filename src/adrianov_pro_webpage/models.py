d336336443cfc0a2b2f09558f529ab4aa803ecc3
adrianoff <adrianov87@gmail.com>
2018-10-20 18:52:26 +0300

    models

--- src/adrianov_pro_webpage/models.py
+++ src/adrianov_pro_webpage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    question = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    content = models.TextField()
--- src/src/settings.py
+++ src/src/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'adrianov_pro_webpage.apps.AdrianovProWebpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'adrianov_pro_webpage/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
