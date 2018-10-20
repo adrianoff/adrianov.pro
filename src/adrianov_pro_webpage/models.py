1ff1ac16659c482eafa44d31c650c3370cc650cc
adrianoff <adrianov87@gmail.com>
2018-10-20 18:55:41 +0300

    test

--- src/adrianov_pro_webpage/models.py
+++ src/adrianov_pro_webpage/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    
+
     def __str__(self):
         return self.name
 
-class Page(models.Model):
+class Page(models.Model): 
     question = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
