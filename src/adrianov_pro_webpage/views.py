cd9db5d4fa02b494f17dce5806e557ba4107b515
adrianoff <adrianov87@gmail.com>
2018-10-20 15:04:31 +0300

    First template

--- src/adrianov_pro_webpage/views.py
+++ src/adrianov_pro_webpage/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Adrianov.pro")
+    template = loader.get_template('main.html')
+    context = {}
+
+    return HttpResponse(template.render(context, request))
